cfd6d5d9d6a735353d52ef01015066140b518221
ivan-iglesias <iiglesiasbilbao@gmail.com>
2020-01-07 18:07:56 +0100

    08. Routing to controllers

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,21 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/*
- * http://localhost:8080/posts/my-first-post
- * http://localhost:8080/posts/my-third-post
- */
-Route::get('/posts/{post}', function ($post) {
-    $posts = [
-        'my-first-post' => 'Hello, this is my first post!',
-        'my-second-post' => 'This is my second post.'
-    ];
-
-    // return view('post', ['post' => $posts[$post] ?? 'Nothing here yet.']);
-
-    if (! array_key_exists($post, $posts)) {
-        abort(404, 'Sorry, that post was not found.');
-    }
-
-    return view('post', ['post' => $posts[$post]]);
-});
+Route::get('/posts/{post}', 'PostsController@show');
