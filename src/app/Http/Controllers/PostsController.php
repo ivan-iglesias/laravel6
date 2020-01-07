9e37ebd13f62904367ee24999cb12881eee61b89
ivan-iglesias <iiglesiasbilbao@gmail.com>
2020-01-07 20:54:45 +0100

    10. Hello Eloquent

--- src/app/Http/Controllers/PostsController.php
+++ src/app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,8 +10,8 @@ class PostsController extends Controller
 {
     public function show($slug)
     {
-        $post = DB::table('posts')->where('slug', $slug)->first();
-
-        return view('post', ['post' => $post]);
+        return view('post', [
+            'post' => Post::where('slug', $slug)->firstOrFail()
+        ]);
     }
 }
