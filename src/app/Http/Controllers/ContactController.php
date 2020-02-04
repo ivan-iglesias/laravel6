7122ad81885fe6d98761fc434167c2ac44e490d8
ivan-iglesias <iiglesiasbilbao@gmail.com>
2020-02-04 22:59:10 +0100

    44. Send HTML Emails Using Mailable Classes

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactMe;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -12,31 +13,14 @@ class ContactController extends Controller
         return view('contact');
     }
 
+    /**
+     * Uso https://mailtrap.io para hacer las pruebas.
+     */
     public function store()
     {
         request()->validate(['email' => 'required|email']);
 
-        /**
-         * En config/mail.php tenemos la configuración de envio de correos. Podemos añadir en
-         * ".env" el MAIL_FROM_ADDRESS para que los correos se envien con la cuenta deseada.
-         *
-         * MAIL_FROM_ADDRESS=admin@example.com
-         *
-         * -------------------------------------------------------
-         * It works!
-         * [2020-02-04 16:53:32] local.DEBUG: Message-ID: <08f4ffd5ea3a62981dacfd7d85bee8f5@swift.generated>
-         * Date: Tue, 04 Feb 2020 16:53:32 +0000
-         * Subject: Hello There
-         * From: Example <admin@example.com>
-         * To: foo@gmail.com
-         * MIME-Version: 1.0
-         * Content-Type: text/plain; charset=utf-8
-         * Content-Transfer-Encoding: quoted-printable
-         */
-        Mail::raw('It works!', function($message) {
-            $message->to(request('email'))
-                    ->subject('Hello There');
-        });
+        Mail::to(request('email'))->send(new ContactMe('shirts'));
 
         return redirect('/contact')
             ->with('message', 'Email sent!');
